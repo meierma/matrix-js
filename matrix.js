ed5d98d37545fdbd01044e5a5f7d8ca4655474d3
Marcel Meier <meierma@uni-bremen.de>
2018-07-30 16:33:58 +0200

    Init function to create canvas for elements

---
+++ matrix.js
@@ -0,0 +1,40 @@
+class Matrix {
+
+    constructor(selector) {
+        this.init(selector);
+    }
+
+    extend(target, source) {
+        // Loop through our object
+        for (var prop in source) {
+            if (target.hasOwnProperty(prop)) {
+                // Push each value from `obj` into `extended`
+                target[prop] = source[prop];
+            }
+        }
+        return target;
+    }
+
+    init(selector) {
+        console.log(selector);
+        
+        var elements = document.querySelectorAll(selector);
+        console.log(elements);
+        [].forEach.call(elements, element => {
+           this.createCanvas(element); 
+        });
+    }
+
+    createCanvas(element){
+        console.log(element);
+        
+        var newCanvas = document.createElement("canvas");
+        element.appendChild(newCanvas);
+
+        newCanvas.style.height = element.offsetHeight+"px";
+        newCanvas.style.width = element.offsetWidth+"px";
+        element.style.overflow = "hidden";
+
+    }
+
+}
